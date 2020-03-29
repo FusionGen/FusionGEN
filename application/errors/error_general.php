4d0eb28fc5fe87fafcc33864ce24c966a32173f0
OMGhixD <omghixd@hotmail.com>
2020-03-29 21:10:23 +0200

    Minimal change of the Error Page Design.

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -9,24 +9,25 @@
 ::webkit-selection{ background-color: #E13300; color: white; }
 
 body {
-	background-color: #fff;
+	background-color: #19191b;
 	margin: 40px;
 	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
+	color: #fff;
 }
 
 a {
-	color: #003399;
+	color: #fff;
 	background-color: transparent;
 	font-weight: normal;
 }
 
 h1 {
-	color: #444;
+	color: #fff;
+	text-transform: uppercase;
+	font-weight: bold;
 	background-color: transparent;
 	border-bottom: 1px solid #D0D0D0;
 	font-size: 19px;
-	font-weight: normal;
 	margin: 0 0 14px 0;
 	padding: 14px 15px 10px 15px;
 }
@@ -35,8 +36,7 @@ code {
 	font-family: Consolas, Monaco, Courier New, Courier, monospace;
 	font-size: 12px;
 	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
+	color: #fff;
 	display: block;
 	margin: 14px 0 14px 0;
 	padding: 12px 10px 12px 10px;
@@ -44,12 +44,12 @@ code {
 
 #container {
 	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
+	border-radius: 2px;
 }
 
 p {
 	margin: 12px 15px 12px 15px;
+	font-style: italic;
 }
 </style>
 </head>
