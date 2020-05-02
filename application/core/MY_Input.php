e9a99e2548732209514d42f9aff24263aca5b5d5
SkyLiner <alexandreluisbarreto@gmail.com>
2020-05-02 16:37:40 +0100

    Updates code layout Updates check on start of the file to be consistent

--- application/core/MY_Input.php
+++ application/core/MY_Input.php
@@ -1,24 +1,25 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 class MY_Input extends CI_Input
 {
-    function _sanitize_globals()
-    {   
+    public function _sanitize_globals()
+    {
         $ignore_csrf = config_item('csrf_ignore');
-        
-        if (is_array($ignore_csrf) && count($ignore_csrf))
-        {
+
+        if (is_array($ignore_csrf) && count($ignore_csrf)) {
             global $URI;
             $haystack = $URI->uri_string();
 
-            foreach($ignore_csrf as $needle)
-            {
-                if (strlen($haystack) >= strlen($needle) && substr($haystack, 0, strlen($needle)) == $needle)
-                {
-                    $this->_enable_csrf = FALSE;
+            foreach ($ignore_csrf as $needle) {
+                if (strlen($haystack) >= strlen($needle) && substr($haystack, 0, strlen($needle)) == $needle) {
+                    $this->_enable_csrf = false;
                     break;
                 }
-            }           
+            }
         }
 
         parent::_sanitize_globals();
