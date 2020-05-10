e842eb2d12f57c20b31b6d78b581d02efe903941
OMGhixD <omghixd@hotmail.com>
2020-05-10 02:17:59 +0200

    Revert "- Fixed missing Avatar.jpg file from commit b971c778"
    This reverts commit 0cb26d08836efd3b3943f3462997fca69cd07b85.

--- application/language/english/ucp.php
+++ application/language/english/ucp.php
@@ -30,7 +30,6 @@ $lang['data_tip_donate'] = "Earn Donation Points by Donating to the Server";
 
 // Avatar
 $lang['change_avatar'] = "Change avatar";
-$lang['upload_avatar'] = "Upload avatar";
 $lang['make_use'] = "We make use of";
 $lang['provides_way'] = "which provides an easy way to maintain your avatars across the web.";
 $lang['to_change'] = "To change your avatar you need to";
