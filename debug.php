82872d6ddffe6e76663ec362fa114236ca3fba99
OMGhixD <omghixd@hotmail.com>
2021-01-21 15:08:54 +0100

    Updated the Debug.php Design and Clearifications

--- debug.php
+++ debug.php
@@ -1,26 +1,30 @@
 <style type="text/css">
 	body {
-		background-color:#eee;
+		background-color:silver;
 		font-family:Arial, Helvetica, sans-serif;
 		font-size:14px;
+        color:#d9534f;
 	}
 
 	h1 {
 		font-size:24px;
 		font-weight:normal;
-		color:#666;
+        color: #363b3e;
+        text-transform:uppercase;
+        font-weight:bolder;
 	}
 
 	div {
-		padding:10px;
-		background-color:green;
-		margin-bottom:5px;
-		color:#fff;
-		border-radius:2px;
+        padding:10px;
+        background-color:#5cb85c;
+        margin-bottom:5px;
+        color:#292b2c;
+        border-radius:2px;
+        font-weight:600;
 	}
 
 	.error {
-		background-color:red;
+		background-color:#d9534f;
 	}
 
 	.realm {
@@ -28,7 +32,7 @@
 	}
 </style>
 
-<h1>Asking if connections are Online or Offline.</h1>
+<h1>Connection Status</h1>
 
 <?php
 
@@ -57,12 +61,12 @@ function select_db($connection, $database)
 $cms_database_connection = connect($db['cms']['hostname'], $db['cms']['username'], $db['cms']['password'], $db['cms']['port'] ?? null);
 select_db($cms_database_connection, $db['cms']['database']);
 
-echo "<div>CMS connection successful</div>";
+echo "<div>[DB] CMS connection successful</div>";
 
 $auth_database_connection = connect($db['account']['hostname'], $db['account']['username'], $db['account']['password'], $db['account']['port'] ?? null);
 select_db($auth_database_connection, $db['account']['database']);
 
-echo "<div>Realmd/logon/auth connection successful</div>";
+echo "<div>[DB] Realmd/logon/auth connection successful</div>";
 
 if ($cms_database_connection) {
     select_db($cms_database_connection, $db['cms']['database']);
@@ -88,7 +92,7 @@ if ($cms_database_connection) {
             $characters_connection[$row['id']] = connect($char['hostname'], $char['username'], $char['password'], $char['port']);
             $world_connection[$row['id']]      = connect($world['hostname'], $world['username'], $world['password'], $world['port']);
 
-            echo "<div class='realm'>Realm #" . $row['id'] . " (" . $row['realmName'] . ") connections (world & characters) successful</div>";
+            echo "<div class='realm'>[DB] Realm #" . $row['id'] . " (" . $row['realmName'] . ") connections (world & characters) successful</div>";
 
             select_db($characters_connection[$row['id']], $row['char_database']);
             select_db($world_connection[$row['id']], $row['char_database']);
@@ -96,7 +100,7 @@ if ($cms_database_connection) {
             try {
                 $connect = fsockopen($row['hostname'], $row['realm_port'], $errno, $errstr, 1.5);
 
-                echo $connect ? "<div>" . $row['realmName'] . " is online</div>" : "<div class='error'>" . $row['realmName'] . " is offline</div>";
+                echo $connect ? "<div>[SERVER] " . $row['realmName'] . " is online</div>" : "<div class='error'>[SERVER] " . $row['realmName'] . " is offline</div>";
 
             } catch (Exception $error) {
                 echo "<div class='error'>" . $error->getMessage() . "</div>";
