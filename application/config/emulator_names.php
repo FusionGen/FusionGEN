bcae4548e8784e12292def7b6d44054ace9babb4
OMGhixD <omghixd@hotmail.com>
2021-01-21 16:05:54 +0100

    And i forgot to remove a comma >_>

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -2,5 +2,5 @@
 
 $emulators = array(
 	"trinity_rbac_ra" => "TrinityCore (3.3.5) with RBAC - connected through RA",
-	"trinity_rbac_soap" => "TrinityCore (3.3.5) with RBAC - connected through SOAP",
+	"trinity_rbac_soap" => "TrinityCore (3.3.5) with RBAC - connected through SOAP"
 );
