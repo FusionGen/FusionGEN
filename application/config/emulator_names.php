497e5c3848c20e72d395f6e5bfe27c86968f68df
Favorit5503 <favorit5503@gmail.com>
2021-04-09 14:30:32 +0200

    Added TrinityCore Shadowlands emulator

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -2,5 +2,6 @@
 
 $emulators = array(
 	"trinity_rbac_soap" => "TrinityCore (3.3.5a) with RBAC - connected through SOAP",
+	"trinity_sl_rbac_soap" => "TrinityCore - Shadowlands (9.x.x) with RBAC - connected through SOAP",
 	"azerothcore_soap" => "AzerothCore (3.3.5a) - connected through SOAP"
 );
