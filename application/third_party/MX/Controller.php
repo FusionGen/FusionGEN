c37158505f53d61202ed3f4ac2ef6d74828ceee9
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2021-04-22 17:32:28 +0300

    Update Controller.php (#76)

--- application/third_party/MX/Controller.php
+++ application/third_party/MX/Controller.php
@@ -121,7 +121,7 @@ class MX_Controller
 			$username = CI::$APP->input->cookie("fcms_username");
 			$password = CI::$APP->input->cookie("fcms_password");
 
-			if($password && column('account', 'password') == 'verifier' && column('account', 'salt')); // Emulator Uses SRP6 Encryption.
+			if($password && column('account', 'password') == 'verifier' && column('account', 'salt')) // Emulator Uses SRP6 Encryption.
 			$password = urldecode(preg_replace('~.(?:fcms_password=([^;]+))?~', '$1', @$_SERVER['HTTP_COOKIE'])); // Fix for HTTP_COOKIE Error.
 
 			if($username && $password)
