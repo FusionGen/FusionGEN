b1e09efa0a6cba4b11d3a29ccda89c18b170d4a8
Err0r <74456323+Err0r1@users.noreply.github.com>
2023-03-17 20:30:45 +0100

    fix: fixed typo in site language change

--- application/modules/admin/controllers/Languages.php
+++ application/modules/admin/controllers/Languages.php
@@ -36,7 +36,7 @@ class Languages extends MX_Controller
 
         requirePermission("changeDefaultLanguage");
 
-        require_once('application/libraries/configeditor.php');
+        require_once('application/libraries/Configeditor.php');
 
         if (!$language || !is_dir("application/language/" . $language)) {
             die("Invalid language");
