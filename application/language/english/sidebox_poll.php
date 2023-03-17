67b386cdbed13582cd85cf8b91aac835668fa4a4
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2023-03-18 01:03:40 +0200

    correct no active poll message

--- application/language/english/sidebox_poll.php
+++ application/language/english/sidebox_poll.php
@@ -13,7 +13,6 @@
  */
 
 $lang['show_results'] = "Results";
-$lang['no_answers'] = "This question has no answers";
 $lang['no_poll'] = "There is currently no active poll";
 $lang['show_options'] = "Options";
 $lang['log_in'] = "Please log in to vote!";
