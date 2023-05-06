7c6e144a178b8fba6373f126747559fc9461e83a
HT Cesta <58153179+htc16@users.noreply.github.com>
2023-05-06 14:47:19 -0300

    Revert "Update Character.php"
    This reverts commit a8ddf967f96932b151432aa73a651df3a6bc09bf.

--- application/modules/character/controllers/Character.php
+++ application/modules/character/controllers/Character.php
@@ -26,7 +26,7 @@ class Character extends MX_Controller
     {
         parent::__construct();
 
-        $this->user->userArea();
+        requirePermission("view");
 
         // Set JS and CSS paths
         $this->js = "modules/character/js/character.js";
@@ -179,8 +179,8 @@ class Character extends MX_Controller
 
                 case "Hunter":
                     if (isset($this->stats['maxpower3'])) {
-                        $this->secondBar = "mana";
-                        $this->secondBarValue = $this->stats['maxpower1'];
+                        $this->secondBar = "focus";
+                        $this->secondBarValue = $this->stats['maxpower3'];
                     } else {
                         $this->secondBar = "mana";
                         $this->secondBarValue = "Unknown";
