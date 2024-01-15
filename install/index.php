d4a43256536c03c731b515e8e392e2bf01b39229
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2024-01-15 02:29:00 +0200

    bump php_version_max to 8.3.1

--- install/index.php
+++ install/index.php
@@ -23,7 +23,7 @@ $zip_success = false;
 $xml_success = false;
 
 $php_version_min = "8.0.0";
-$php_version_max = "8.3.0";
+$php_version_max = "8.3.1";
 $current_php_version = phpversion();
 
 if ($current_php_version <= $php_version_max && $current_php_version >= $php_version_min) {
