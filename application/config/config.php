eeca7c8c9f6da268e8a6d8ef275c24f06fdfcf77
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2024-03-20 14:07:36 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -147,7 +147,7 @@ $config['composer_autoload'] = FCPATH . 'vendor/autoload.php';
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = '‘a-z 0-9~%.:\_\=+%\&-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 
 /*
 |--------------------------------------------------------------------------
