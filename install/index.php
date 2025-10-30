048d81282573de342c72027bdb975240467491fc
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2025-10-30 14:06:20 +0200

    Update maximum PHP version to 8.4.14

--- install/index.php
+++ install/index.php
@@ -19,7 +19,7 @@ $zip_success = false;
 $xml_success = false;
 
 $php_version_min = "8.0.0";
-$php_version_max = "8.4.13";
+$php_version_max = "8.4.14";
 $current_php_version = phpversion();
 
 if ($current_php_version <= $php_version_max && $current_php_version >= $php_version_min) {
