84f09e0e3268963072e438148a6e53657a94a911
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2025-12-20 21:33:22 +0200

    Refactor index.php part 1

--- index.php
+++ index.php
@@ -21,10 +21,9 @@ if (file_exists("install") && !file_exists("install/.lock"))
  *     production
  *
  * NOTE: If you change these, also change the error_reporting() code below
- *
  */
-
 	define('ENVIRONMENT', 'development');
+
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -33,61 +32,53 @@ if (file_exists("install") && !file_exists("install/.lock"))
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and production will hide them.
  */
-
-if (defined('ENVIRONMENT'))
+switch (ENVIRONMENT)
 {
-	switch (ENVIRONMENT)
-	{
-		case 'development':
-			ini_set('display_errors', '1');
-			error_reporting(E_ALL & ~E_DEPRECATED);
-		break;
+	case 'development':
+		ini_set('display_errors', '1');
+		error_reporting(E_ALL & ~E_DEPRECATED);
+	break;
 
-		case 'testing':
-		case 'production':
-		ini_set('display_errors', '0');
-		if (version_compare(PHP_VERSION, '8.4', '>=')) {
-			error_reporting(E_ALL & ~E_DEPRECATED);
-		} else {
-			error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
-		}
-		break;
-
-		default:
-			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-			echo 'The application environment is not set correctly.';
-			exit(1); // EXIT_ERROR
+	case 'testing':
+	case 'production':
+	ini_set('display_errors', '0');
+	if (version_compare(PHP_VERSION, '8.4', '>=')) {
+		error_reporting(E_ALL & ~E_DEPRECATED);
+	} else {
+		error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 	}
+	break;
+
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
 }
-
-date_default_timezone_set("Europe/Amsterdam");
-
 
 /*
  *---------------------------------------------------------------
- * SYSTEM FOLDER NAME
+ * SYSTEM DIRECTORY NAME
  *---------------------------------------------------------------
  *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same  directory
- * as this file.
- *
+ * This variable must contain the name of your "system" directory.
+ * Set the path if it is not in the same directory as this file.
  */
 	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
- * APPLICATION FOLDER NAME
+ * APPLICATION DIRECTORY NAME
  *---------------------------------------------------------------
  *
  * If you want this front controller to use a different "application"
- * folder then the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server.  If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
+ * directory than the default one you can set its name here. The directory
+ * can also be renamed or relocated anywhere on your server. If you do,
+ * use an absolute (full) server path.
+ * For more info please see the user guide:
+ *
+ * https://codeigniter.com/userguide3/general/managing_apps.html
  *
  * NO TRAILING SLASH!
- *
  */
 	$application_folder = 'application';
 
@@ -98,24 +89,23 @@ date_default_timezone_set("Europe/Amsterdam");
  *
  * Normally you will set your default controller in the routes.php file.
  * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here.  For most applications, you
+ * specific controller class/function here. For most applications, you
  * WILL NOT set your routing here, but it's an option for those
  * special instances where you might want to override the standard
  * routing in a specific front controller that shares a common CI installation.
  *
- * IMPORTANT:  If you set the routing here, NO OTHER controller will be
+ * IMPORTANT: If you set the routing here, NO OTHER controller will be
  * callable. In essence, this preference limits your application to ONE
- * specific controller.  Leave the function name blank if you need
+ * specific controller. Leave the function name blank if you need
  * to call functions dynamically via the URI.
  *
  * Un-comment the $routing array below to use this feature
- *
  */
-	// The directory name, relative to the "controllers" folder.  Leave blank
-	// if your controller is not in a sub-folder within the "controllers" folder
+	// The directory name, relative to the "controllers" directory.  Leave blank
+	// if your controller is not in a sub-directory within the "controllers" one
 	// $routing['directory'] = '';
 
-	// The controller class file name.  Example:  Mycontroller
+	// The controller class file name.  Example:  mycontroller
 	// $routing['controller'] = '';
 
 	// The controller function you wish to be called.
@@ -135,7 +125,6 @@ date_default_timezone_set("Europe/Amsterdam");
  * config values.
  *
  * Un-comment the $assign_to_config array below to use this feature
- *
  */
 	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
@@ -187,19 +176,14 @@ date_default_timezone_set("Europe/Amsterdam");
 	// The name of THIS file
 	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// The PHP file extension
-	// this global constant is deprecated.
-	define('EXT', '.php');
+	// Path to the system directory
+	define('BASEPATH', $system_path);
 
-	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
+	// Path to the front controller (this file) directory
+	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-	// Path to the front controller (this file)
-	define('FCPATH', str_replace(SELF, '', __FILE__));
-
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
+	// Name of the "system" directory
+	define('SYSDIR', basename(BASEPATH));
 
 	// The path to the "application" folder
 	if (is_dir($application_folder))
