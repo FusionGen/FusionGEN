632975119162127156b07d24fa1e678e259d9208
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2026-01-21 01:22:53 +0200

    Update PHP version maximum to 8.5.2

--- install/index.php
+++ install/index.php
@@ -20,7 +20,7 @@ $zip_success = false;
 $xml_success = false;
 
 $php_version_min = "8.1.0";
-$php_version_max = "8.5.1";
+$php_version_max = "8.5.2";
 $current_php_version = phpversion();
 
 if ($current_php_version <= $php_version_max && $current_php_version >= $php_version_min) {
